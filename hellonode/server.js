2c27ee1112a8694476bd0206dc6384b41f9a111b
John Costa <john.costa@gmail.com>
2012-10-04 11:16:31 -0400

    make the message node specific

--- hellonode/server.js
+++ hellonode/server.js
@@ -1,6 +1,6 @@
 require('http').createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  output = "Hello World!\n";
+  output = "Hello World with node!\n";
   for (k in request.headers) {
     output += k + '=' + request.headers[k] + '\n';
   }
